3f97084ef4378072e60f9f67fcf68c56e3dcc285
ahellerjones <hellerar@vt.edu>
2021-12-06 12:37:55 -0500

    Fixed second screen

--- app3.py
+++ app3.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QPushButton
 
 
 
@@ -20,62 +20,82 @@ class TableModel(QtCore.QAbstractTableModel):
             # .column() indexes into the sub-list
             return self._data[index.row()][index.column()]
 
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
+
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return len(self._data[0])
+
+'''
+Makes Label and table pairs
+'''
+class LabelTable(QWidget): 
+    def __init__(self, label, tableData, parent=None):
+        super().__init__(parent)
+        self.lay = QVBoxLayout()
+        self.setLayout(self.lay)
+        self.lay.addWidget(label)
+        table = QtWidgets.QTableView()
+        model = TableModel(tableData)
+        table.setModel(model)
+        self.lay.addWidget(table)
 
 ''' 
 holds the main window
 '''
 class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
         table = QtWidgets.QTableView()
-        m = QHBoxLayout()
+        m = QVBoxLayout()
         self.setLayout(m)
-        data = [
+        self.leftTable = [
           ['DogsSuck'],
           ['RandomCat'],
           ['jimbob420'],
         ]
-
-        model = TableModel(data)
-        table.setModel(model)
-
-        left = QWidget()
+        leftTable = LabelTable(QLabel('Found Users: '), self.leftTable)
+        left = leftTable
         main = QWidget()
         mainLayout = QHBoxLayout()
         main.setLayout(mainLayout)
 
-        leftLayout = QVBoxLayout()
-        left.setLayout(leftLayout)
-        leftLayout.addWidget(QLabel('Found Users: '))
-        leftLayout.addWidget(table)
     
         right = QWidget()
         
-        rightPanel = QHBoxLayout()
+        rightPanel = QVBoxLayout()
         right.setLayout(rightPanel)
-        tableTR = QtWidgets.QTableView()
+
+
+
         
-        TRdata = [
+        self.TRdata = [
           ['DogsSuck'],
           ['RandomCat'],
           ['jimbob420'],
         ]
     
+        self.BRdata = [
+          ['DogsSuck'],
+          ['RandomCat'],
+          ['jimbob420'],
+        ]
 
-        rightModel = TableModel(TRdata)
-        tableTR.setModel(rightModel)
-        TRwidget = QWidget()
-        TRlay = QVBoxLayout()
-        TRlay.addWidget(QLabel('Top Users: '))
-        TRlay.addWidget(tableTR)
+        TRwidget = LabelTable(QLabel('Top Users: '), self.TRdata)
+        BRwidget = LabelTable(QLabel('Top Posts: '), self.BRdata)
         rightPanel.addWidget(TRwidget)
-        
+        rightPanel.addWidget(BRwidget)
+
         
         mainLayout.addWidget(left)
         mainLayout.addWidget(right)
         m.addWidget(main)
-        #self.setCentralWidget(main)
+        self.searchAgain = QPushButton('Search Again')
+        m.addWidget(self.searchAgain)
 
 
 app=QtWidgets.QApplication(sys.argv)
